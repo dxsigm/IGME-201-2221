8f45f43bcdba2aab89ac30b1f7e80187eac028fb
dxsigm <dxsigm@rit.edu>
2022-08-22 13:07:10 -0400

    Session 1-1

---
+++ Week 01/Session 1/FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColorAndNumber
+{
+    // Class: Program
+    // Author: David Schuh
+    // Purpose: Console Read/Write and Exception-handling exercise
+    // Restrictions: None
+    // BE SURE TO ADD THE "static" KEYWORD TO ALL CLASS DEFINITIONS DURING UNIT 1!
+    static internal class Program
+    {
+        // Method: Main
+        // Purpose: Prompt the user for their favorite color and number
+        //          Output their favorite color (in limited text colors) their favorite number of times
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // prompt the user for their favorite color
+
+            // get the user's input and store it in a variable
+
+            // prompt the user for their favorite number
+
+            // validate that the user entered a valid number
+
+            // change the output text color to match their favorite color
+
+            // output their favorite color the number of times as their favorite number
+        }
+    }
+}
