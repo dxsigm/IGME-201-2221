371b74146ad2c3412426d68879ff4a3ba9b3b807
dxsigm <dxsigm@rit.edu>
2022-11-04 13:54:38 -0400

    Session 11-3

---
+++ Week 11/Session 3/EditPerson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PeopleLib;
+
+namespace EditPerson
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // always call these 2 commands first for Windows Forms Applications
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Person student = new Student();
+            PersonEditForm editForm = new PersonEditForm(student, null);
+            
+            Application.Run(editForm);
+        }
+    }
+}
