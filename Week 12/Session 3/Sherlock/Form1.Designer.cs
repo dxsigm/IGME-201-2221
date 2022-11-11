d99963b88394e7c3e851bfdf3d43704f0360f8c1
dxsigm <dxsigm@rit.edu>
2022-11-11 13:32:31 -0500

    Session 12-3

---
+++ Week 12/Session 3/Sherlock/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Sherlock
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.quickLabel = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.sadPictureBox = new System.Windows.Forms.PictureBox();
+            this.happyPictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sadPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.happyPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // quickLabel
+            // 
+            this.quickLabel.AutoSize = true;
+            this.quickLabel.Location = new System.Drawing.Point(28, 36);
+            this.quickLabel.Name = "quickLabel";
+            this.quickLabel.Size = new System.Drawing.Size(225, 13);
+            this.quickLabel.TabIndex = 0;
+            this.quickLabel.Text = "The quick brown fox jumped over the lazy dog";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(31, 52);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(237, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.webBrowser);
+            this.groupBox.Location = new System.Drawing.Point(403, 36);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(371, 316);
+            this.groupBox.TabIndex = 2;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "http://https://people.rit.edu/dxsigm/sherlock.html";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(3, 16);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(365, 297);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(690, 395);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.ForeColor = System.Drawing.Color.Red;
+            this.timerLabel.Location = new System.Drawing.Point(91, 96);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(86, 31);
+            this.timerLabel.TabIndex = 4;
+            this.timerLabel.Text = "label1";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            // 
+            // sadPictureBox
+            // 
+            this.sadPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("sadPictureBox.Image")));
+            this.sadPictureBox.Location = new System.Drawing.Point(31, 176);
+            this.sadPictureBox.Name = "sadPictureBox";
+            this.sadPictureBox.Size = new System.Drawing.Size(237, 173);
+            this.sadPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sadPictureBox.TabIndex = 5;
+            this.sadPictureBox.TabStop = false;
+            // 
+            // happyPictureBox
+            // 
+            this.happyPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("happyPictureBox.Image")));
+            this.happyPictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("happyPictureBox.InitialImage")));
+            this.happyPictureBox.Location = new System.Drawing.Point(31, 179);
+            this.happyPictureBox.Name = "happyPictureBox";
+            this.happyPictureBox.Size = new System.Drawing.Size(237, 173);
+            this.happyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.happyPictureBox.TabIndex = 6;
+            this.happyPictureBox.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.happyPictureBox);
+            this.Controls.Add(this.sadPictureBox);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.quickLabel);
+            this.Name = "Form";
+            this.Text = "Sherlock";
+            this.groupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sadPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.happyPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label quickLabel;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox sadPictureBox;
+        private System.Windows.Forms.PictureBox happyPictureBox;
+    }
+}
+
---
+++ Week 12/Session 3/Sherlock/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sherlock
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+            // timerLabel is not visible
+            this.timerLabel.Visible = false;
+
+            // initialize the timerLabel to 20
+            this.timerLabel.Text = "20";
+
+            // associate a KeyPress delegate method with the textBox
+            this.textBox.KeyPress += new KeyPressEventHandler(TextBox__KeyPress);
+
+            // timer initialize to 1000ms
+            this.timer.Interval = 1000;
+
+            // associate a Tick delegate method for each time the timer lapses
+            this.timer.Tick += new EventHandler(Timer__Tick);
+
+            // suppress any web/script warnings in the webBrowser control
+            this.webBrowser.ScriptErrorsSuppressed = true;
+
+            // associate a WebBrowserDocumentCompleted delegate method with the wb
+            this.webBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser__DocumentCompleted);
+
+            // navigate the wb to url
+            this.webBrowser.Navigate(""https://people.rit.edu/dxsigm/sherlock.html");
+
+            // hide the sad pb
+            this.sadPictureBox.Visible = false;
+
+            // hide the happy pb
+            this.happyPictureBox.Visible = false;
+
+            // disable the exit button
+            this.exitButton.Enabled = false;
+
+            // associate a Click delegate method with the exit button
+            this.exitButton.Click += new EventHandler(ExitButton__Click);
+
+            // could be written more succinctly, but then you do not see the delegate method signature
+            //this.exitButton.Click += ExitButton__Click;
+
+        }
+
+        private void TextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // if timerLabel is still "20" then start the timer and show the timerLabel
+            if (this.timerLabel.Text == "20")
+            {
+                this.timer.Start();
+                this.timerLabel.Visible = true;
+            }
+
+            // if they typed the correct character
+            if (this.quickLabel.Text[this.textBox.TextLength] == e.KeyChar)
+            {
+                // tell .NET to add it to the TextBox
+                // (we are not handling it)
+                e.Handled = false;
+
+                // hide the sad face
+                this.sadPictureBox.Visible = false;
+
+                // show the happy face
+                this.happyPictureBox.Visible = true;
+
+                // if they typed the whole sentence
+                if (this.textBox.Text + e.KeyChar == this.quickLabel.Text)
+                {
+                    //enable the Exit button
+                    this.exitButton.Enabled = true;
+
+                    // remove the KeyPress event handler from the TextBox
+                    this.textBox.KeyPress -= TextBox__KeyPress;
+                }
+            }
+            // else they did not type the correct character
+            else
+            {
+                // tell .NET that we are handling the keypress and it should not show it
+                e.Handled = true;
+
+                // show the sad face
+                this.sadPictureBox.Visible = true;
+
+                // hide the happy face
+                this.happyPictureBox.Visible = false;
+            }
+        }
+
+        private void Timer__Tick(object sender, EventArgs e)
+        {
+            // if the timer Text is "1"
+            {
+                // stop the timer
+                // clear the TextBox
+                // hide the timer label
+                // reset the timer label to "20"
+                // hide the sad face
+                // hide the happy face
+            // else
+                // decrement the textLabel by 1
+            }
+        }
+
+        private void ExitButton__Click(object sender, EventArgs e)
+        {
+            // Exit the application
+        }
+
+        private void WebBrowser__DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            // get the collection of anchor elements
+            // associate a click delegate method with each anchor tag
+            // remove this event handler from the wb
+        }
+
+        private void Anchor__Click(object sender, HtmlElementEventArgs e)
+        {
+            // // fetch the HtmlElement that was clicked
+            // if the text contains "again"
+            //    // change the text and the style to the last phrase
+            //    // remove the delegate from the element
+            // else if the text contains "clicked"
+            //    // change the text and style to the second phrase
+            // else
+            //    // change the text to the first phrase
+
+        }
+    }
+}
