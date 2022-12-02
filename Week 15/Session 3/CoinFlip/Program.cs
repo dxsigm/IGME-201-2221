f29cdb75d69ace64eb4b034687725c921ea3a4fe
dxsigm <dxsigm@rit.edu>
2022-12-02 13:10:56 -0500

    Session 15-3

--- Week 15/Session 3/CoinFlip/Program.cs
+++ Week 15/Session 3/CoinFlip/Program.cs
@@ -27,10 +27,33 @@ namespace CoinFlip
         };
 
 
+        /// to add cost to the matrix, we can change it to int and use -1 
+        /// to indicate nodes are not adjacent (if the costs are only positive)
+        static int[,] mGraph = new int[,]
+        {
+                   // "TTT"     "TTH"    THT       THH       HTT      HTH        HHT       HHH
+         /* TTT */  { -1   ,       1    , 2 ...
+        };
+
+
+        /// the cost can be added using tuples (neighbor,cost)
         static int[][] lGraph = new int[][]
         {
+            /* TTT */ new int[] {(1,1), (2,2), (4,4) },
+            /* TTH */ new int[] { (0,1), (3,3) },
+            /* THT */ new int[] { 0 },
+            /* THH */ new int[] { 1, 7 },
+            /* HTT */ new int[] { 0, 6 },
+            /* HTH */ new int[] { 7 },
+            /* HHT */ new int[] { 4, 7 },
+            /* HHH */ new int[] { 3, 5, 6 }
+        };
+
+        /// or defining a parallel cost adjacency list
+        static int[][] lCost = new int[][]
+        {
             /* TTT */ new int[] {1, 2, 4 },
-            /* TTH */ new int[] { 0, 3 },
+            /* TTH */ new int[] { 5, 3},
             /* THT */ new int[] { 0 },
             /* THH */ new int[] { 1, 7 },
             /* HTT */ new int[] { 0, 6 },
@@ -73,10 +96,74 @@ namespace CoinFlip
 
         static Object lockObject = new Object();
 
+        static List<Node> game = new List<Node>();
+
 
         static void Main(string[] args)
         {
             Random rand = new Random();
+            Node node;
+
+            // populate the game List<Node> to calculate the shortest path from 2 to 5
+            node = new Node(0);
+            game.Add(node);
+
+            node = new Node(1);
+            game.Add(node);
+
+            node = new Node(2);
+            game.Add(node);
+
+            node = new Node(3);
+            game.Add(node);
+
+            node = new Node(4);
+            game.Add(node);
+
+            node = new Node(5);
+            game.Add(node);
+
+            node = new Node(6);
+            game.Add(node);
+
+            node = new Node(7);
+            game.Add(node);
+
+            game[0].AddEdge(1, game[1]);
+            game[0].AddEdge(2, game[2]);
+            game[0].AddEdge(4, game[4]);
+            game[0].edges.Sort();
+
+            game[1].AddEdge(1, game[0]);
+            game[1].AddEdge(3, game[3]);
+            game[1].edges.Sort();
+
+            game[2].AddEdge(2, game[0]);
+            game[2].edges.Sort();
+
+            game[3].AddEdge(3, game[1]);
+            game[3].AddEdge(7, game[7]);
+            game[3].edges.Sort();
+
+            game[4].AddEdge(4, game[0]);
+            game[4].AddEdge(6, game[6]);
+            game[4].edges.Sort();
+
+            game[5].AddEdge(7, game[7]);
+            game[5].edges.Sort();
+
+            game[6].AddEdge(6, game[4]);
+            game[6].AddEdge(7, game[7]);
+            game[6].edges.Sort();
+
+            game[7].AddEdge(7, game[3]);
+            game[7].AddEdge(7, game[5]);
+            game[7].AddEdge(7, game[6]);
+            game[7].edges.Sort();
+
+            // get the shortest path
+            List<Node> shortestPath = GetShortestPathDijkstra();
+
 
             // the current string representation of the coin state
             string sState;
@@ -88,11 +175,11 @@ namespace CoinFlip
 
             nState = 2;
 
-            while(true)
+            while (true)
             {
                 nState = rand.Next(0, 8);
 
-                if( nState != 5 )
+                if (nState != 5)
                 {
                     break;
                 }
@@ -101,7 +188,7 @@ namespace CoinFlip
             Thread t = new Thread(DFS);
             t.Start();
 
-            while( nState != 5)
+            while (nState != 5)
             {
                 // convert the current numeric state to a string
                 sState = NState2SState(nState);
@@ -113,7 +200,7 @@ namespace CoinFlip
                 Console.Write("Enter the desired state: ");
                 //sUserState = Console.ReadLine().ToUpper();
 
-                lock(lockObject)
+                lock (lockObject)
                 {
                     bWaitingForMove = true;
                 }
@@ -138,11 +225,11 @@ namespace CoinFlip
                 int[] thisStateList = lGraph[nState];
                 bool bValid = false;
 
-                if( thisStateList != null )
+                if (thisStateList != null)
                 {
-                    foreach(int n in thisStateList)
+                    foreach (int n in thisStateList)
                     {
-                        if( n == nUserState)
+                        if (n == nUserState)
                         {
                             bValid = true;
                             nState = nUserState;
@@ -152,7 +239,7 @@ namespace CoinFlip
                     }
                 }
 
-                if( !bValid)
+                if (!bValid)
                 {
                     Console.WriteLine("That is an invalid move.");
                 }
@@ -234,5 +321,160 @@ namespace CoinFlip
                 }
             }
         }
+
+        /****************************************************************************************
+        The Dijkstra algorithm was discovered in 1959 by Edsger Dijkstra.
+        This is how it works:
+        
+        1. From the start node, add all connected nodes to a priority queue.
+        2. Sort the priority queue by lowest cost and make the first node the current node.
+           For every child node, select the shortest path to start.
+           When all edges have been investigated from a node, that node is "Visited" 
+           and you don´t need to go there again.
+        3. Add each child node connected to the current node to the priority queue.
+        4. Go to step 2 until the queue is empty.
+        5. Recursively create a list of each node that defines the shortest path 
+           from end to start.
+        6. Reverse the list and you have found the shortest path
+        
+        In other words, recursively for every child of a node, measure its distance to the start. 
+        Store the distance and what node led to the shortest path to start. When you reach the end 
+        node, recursively go back to the start the shortest way, reverse that list and you have the 
+        shortest path.
+        ******************************************************************************************/
+
+        static public List<Node> GetShortestPathDijkstra()
+        {
+            DijkstraSearch();
+            List<Node> shortestPath = new List<Node>();
+            shortestPath.Add(game[5]);
+            BuildShortestPath(shortestPath, game[5]);
+            shortestPath.Reverse();
+            return (shortestPath);
+        }
+
+        static private void BuildShortestPath(List<Node> list, Node node)
+        {
+            if (node.nearestToStart == null)
+            {
+                return;
+            }
+
+            list.Add(node.nearestToStart);
+            BuildShortestPath(list, node.nearestToStart);
+        }
+
+
+        static private int NodeOrderBy(Node n)
+        {
+            return n.minCostToStart;
+        }
+
+        static private void DijkstraSearch()
+        {
+            Node start = game[2];
+
+            start.minCostToStart = 0;
+            List<Node> prioQueue = new List<Node>();
+            prioQueue.Add(start);
+
+            //Func<Node, int> nodeOrderBy = new Func<Node, int>(NodeOrderBy);
+            Func<Node, int> nodeOrderBy = NodeOrderBy;
+
+            do
+            {
+                // sort our prioQueue by minCostToStart
+                // option #1, use .Sort() which sorts in place
+                prioQueue.Sort();
+
+                // option #2, use .OrderBy() with a delegate method or lambda expression 
+                // the next 6 lines are equivalent from descriptive to abbreviated:
+                prioQueue = prioQueue.OrderBy(nodeOrderBy).ToList();
+                prioQueue = prioQueue.OrderBy(delegate (Node n) { return n.minCostToStart; }).ToList();
+                prioQueue = prioQueue.OrderBy((Node n) => { return n.minCostToStart; }).ToList();
+                prioQueue = prioQueue.OrderBy((n) => { return n.minCostToStart; }).ToList();
+                prioQueue = prioQueue.OrderBy((n) => n.minCostToStart).ToList();
+                prioQueue = prioQueue.OrderBy(n => n.minCostToStart).ToList();
+
+                Node node = prioQueue.First();
+                prioQueue.Remove(node);
+                foreach (Edge cnn in //node.edges)
+                         node.edges.OrderBy(delegate (Edge n) { return n.cost; }))
+                {
+                    Node childNode = cnn.connectedNode;
+                    if (childNode.visited)
+                    {
+                        continue;
+                    }
+
+                    if (childNode.minCostToStart == int.MaxValue ||
+                        node.minCostToStart + cnn.cost < childNode.minCostToStart)
+                    {
+                        childNode.minCostToStart = node.minCostToStart + cnn.cost;
+                        childNode.nearestToStart = node;
+                        if (!prioQueue.Contains(childNode))
+                        {
+                            prioQueue.Add(childNode);
+                        }
+                    }
+                }
+
+                node.visited = true;
+
+                // if we reeached our target
+                if (node == game[5])
+                {
+                    return;
+                }
+            } while (prioQueue.Any());
+        }
     }
+
+    public class Node : IComparable<Node>
+    {
+        // data
+        public int nState;
+
+        // list of edges
+        public List<Edge> edges = new List<Edge>();
+
+        public int minCostToStart;
+        public Node nearestToStart;
+        public bool visited;
+
+        public Node(int nState)
+        {
+            this.nState = nState;
+            this.minCostToStart = int.MaxValue;
+        }
+
+        public void AddEdge(int cost, Node connection)
+        {
+            Edge e = new Edge(cost, connection);
+            edges.Add(e);
+        }
+
+        public int CompareTo(Node n)
+        {
+            return this.minCostToStart.CompareTo(n.minCostToStart);
+        }
+    }
+
+    public class Edge : IComparable<Edge>
+    {
+        public int cost;
+        public Node connectedNode;
+
+        public Edge(int cost, Node connectedNode)
+        {
+            this.cost = cost;
+            this.connectedNode = connectedNode;
+        }
+
+        public int CompareTo(Edge e)
+        {
+            return this.cost.CompareTo(e.cost);
+        }
+    }
+
 }
